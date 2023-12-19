003835db842003717946ff2d8ccaf3595d67770e
asaltcode <elangovanr558@gmail.com>
2023-12-19 12:21:23 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"; //rfce
-
+import Card from "./components/Card";
 function App() {
   let data = [
     {
@@ -46,10 +46,8 @@ function App() {
     dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!`,
     },
   ];
-  return (
-    <>
+  return   <>
       <div className="container">
-        {/* <!-- Page Heading --> */}
         <h1 className="my-4">
           Page Heading
           <small>Secondary Text</small>
@@ -57,30 +55,11 @@ function App() {
 
         <div className="row">
           {data.map((e, i) => {
-            return (
-              <div className="col-lg-4 col-sm-6 mb-4" key={i}>
-                <div className="card h-100">
-                  <a href="#">
-                    <img
-                      className="card-img-top"
-                      src="https://via.placeholder.com/700x400"
-                      alt=""
-                    />
-                  </a>
-                  <div className="card-body">
-                    <h4 className="card-title">
-                      <a href="#">{e.title}</a>
-                    </h4>
-                    <p className="card-text">{e.description}</p>
-                  </div>
-                </div>
-              </div>
-            );
+           return <Card cardData={e} key={i} />
           })}
         </div>
       </div>
     </>
-  );
 }
 
 export default App;
